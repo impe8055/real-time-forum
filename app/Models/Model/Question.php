8222aff734a60b62df604d94300ab1ced6a82341
impe8055 <you@example.com>
2021-01-01 13:36:08 +0900

    relationships

--- app/Models/Model/Question.php
+++ app/Models/Model/Question.php
@@ -5,7 +5,26 @@ namespace App\Models\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Question extends Model
 {
-    use HasFactory;
+    public function user()
+    {
+        return $this->belogsTo(User::class);
+
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+
+    }
+
+    public function category()
+    {
+        return $this->belogsTo(Category::class);
+
+    }
+
+
 }
