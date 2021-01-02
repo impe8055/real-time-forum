eff70c2b04bb78850fde162f75f1f59fd7093b87
impe8055 <you@example.com>
2021-01-02 22:06:45 +0900

    CRUD for Question with QuestionResource

--- app/Models/Model/Question.php
+++ app/Models/Model/Question.php
@@ -4,8 +4,27 @@ namespace App\Models\Model;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Question extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
